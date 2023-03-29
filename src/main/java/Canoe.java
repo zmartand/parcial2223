3d0d46bdb818ea703603ea73e3eaf32de56444a5
Zara Martín <zmartand@myuax.es>
2023-03-29 19:12:43 +0200

    commit

--- src/main/java/Canoe.java
+++ src/main/java/Canoe.java
@@ -3,7 +3,7 @@ import java.awt.*;
 public class Canoe extends Ship {
     private static final int SIZE = 1;
 
-    public Canoe(Point startPoint) {
+    public Canoe(Point startPoint, Point endPoint) {
         super(startPoint, startPoint);
         if (getSize() != SIZE) {
             throw new IllegalArgumentException("El tamaño de la canoa debe ser 1.");
