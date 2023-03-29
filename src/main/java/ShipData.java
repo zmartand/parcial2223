a77193b8e49e3e55765c83c6e2323e190d4c66e1
Zara Martín <zmartand@myuax.es>
2023-03-29 19:13:39 +0200

    Ejercicio 7 Clase ShipData

---
+++ src/main/java/ShipData.java
@@ -0,0 +1,13 @@
+public class ShipData {
+    private String name;
+    private String type;
+    private int number;
+    private int level;
+
+    public ShipData(String name, String type, int number, int level) {
+        this.name = name;
+        this.type = type;
+        this.number = number;
+        this.level = level;
+    }
+}
